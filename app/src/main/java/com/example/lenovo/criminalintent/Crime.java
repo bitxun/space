0f2766e62d3f97e101bd546b9640dee02c5f5a7e
YX <cnycyx@gmail.com>
2016-11-28 15:58:35 +0800

    12312

---
+++ app/src/main/java/com/example/lenovo/criminalintent/Crime.java
@@ -0,0 +1,46 @@
+package com.example.lenovo.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by lenovo on 2016/11/18.
+ */
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mmDate;
+    private boolean mSolved;
+    public Crime() {
+// Generate unique identifier
+        mId = UUID.randomUUID();
+        mmDate = new Date();
+    }
+
+
+    public Date getDate() {
+        return mmDate;
+    }
+    public void setDate(Date date) {
+        mmDate = date;
+    }
+    public boolean isSolved() {
+        return mSolved;
+    }
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
